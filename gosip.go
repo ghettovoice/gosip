562bb83449e45facee5ec1e7287681bf68f02332
Vladimir Vershinin <ghettovoice@gmail.com>
2019-12-10 19:03:51 +0300

    Remove global default server

--- gosip.go
+++ gosip.go
@@ -2,21 +2,3 @@ package gosip
 
 // Version is the current gosip package version
 var Version = "0.0.0"
-
-var (
-	defaultServer *Server
-)
-
-// DefaultServer returns auto created default SIP server
-func DefaultServer() *Server {
-	return defaultServer
-}
-
-// Listen starts SIP stack
-func Listen(network string, listenAddr string) error {
-	if defaultServer == nil {
-		defaultServer = NewServer(nil)
-	}
-
-	return defaultServer.Listen(network, listenAddr)
-}
