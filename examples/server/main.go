41c671b46abb4f903ccc5da983ceeace8da6f4e1
cloudwebrtc <duanweiwei1982@gmail.com>
2020-12-19 00:17:46 +0800

    Add tls example.

---
+++ examples/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ghettovoice/gosip"
+	"github.com/ghettovoice/gosip/log"
+	"github.com/ghettovoice/gosip/transport"
+)
+
+var (
+	logger log.Logger
+)
+
+func init() {
+	logger = log.NewDefaultLogrusLogger().WithPrefix("Server")
+}
+
+func main() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	srvConf := gosip.ServerConfig{}
+	srv := gosip.NewServer(srvConf, nil, nil, logger)
+	srv.Listen("tls", "0.0.0.0:5061", &transport.Options{CertFile: "certs/cert.pem", KeyFile: "certs/key.pem"})
+
+	<-stop
+
+	srv.Shutdown()
+}
